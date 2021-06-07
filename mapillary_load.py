5445400e57c52332fbffd003f96fc445b4fa9488
DeviSasikumar-TomTom <devi.sasikumar@tomtom.com>
2021-06-07 17:45:50 +0200

    new

--- mapillary_load.py
+++ mapillary_load.py
@@ -1,43 +1,53 @@
-import os.path as osp
-import numpy as np
-import random
-#import matplotlib.pyplot as plt
-import collections
-import torch
-import torchvision.transforms as transforms
+import torch.utils.data as data
 import cv2
-from torch.utils import data
-from PIL import Image
-import glob
 import yaml
-import sys
-import os
-import argparse
+import glob
+import os.path as osp
+import random
+import numpy as np
+import utils
+import transformations1
+from transformations1 import RandomCrop
+import transformations1 as tr
+import torchvision.transforms as transforms
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))
-import transformations as tr
 
 
-class MVD():
-    def __init__(self, root, label_mapping_config, training=True):
+class Mapillary():
+
+    def __init__(self,
+                 root,
+                 label_map,
+                 download=False,
+                 training=True):
+
         self.root = root
-
-        #map_root_image = os.path.join(self.root,'images')
-        self.files = []
         self.training = training
-        label_mapping_yaml = open(label_mapping_config)
-        parsed_label_mapping_yaml_file = yaml.load(label_mapping_yaml, Loader=yaml.FullLoader)
+        self.files = []
+        self.img_file = []
+        self.label_file = []
+        label_map_yaml = open(label_map)
+        parsed_label_mapping_yaml_file = yaml.load(label_map_yaml, Loader=yaml.FullLoader)
         self.label_mapping = parsed_label_mapping_yaml_file["label_mapping"]
 
 
         for file_path in glob.glob(osp.join(root, 'images/*.jpg')):
             filename = osp.basename(file_path).split('.')[0]
             img_file = file_path
-            label_file = osp.join(root, 'instances', filename + '.png')
+            self.img_file.append(img_file)
+
+            image_root = osp.join(root,'images')
+        for filepath1 in glob.glob(osp.join(root, 'instances/*.png')):
+            filename1 = osp.basename(filepath1).split('.')[0]
+            label_file = filepath1
+
+
+            self.label_file.append(label_file)
+
             self.files.append({
                 "img": img_file,
-                "label": label_file,
-            })
+                 "label": label_file
+               })
 
     def __len__(self):
         return len(self.files)
@@ -51,62 +61,63 @@ class MVD():
         label = np.uint8(label)
         sample = {"image": image, "label": label}
 
+       #image1 = Image.open(self.img_file[index]).convert('RGB')
+       #label1 = Image.open(self.label_file[index])
+
         if self.training:
             return self.transforms_tr(sample)
         else:
             return self.transforms_valid(sample)
+
+
+
+    def __len__(self):
+        return len(self.img_file)
 
     def transforms_tr(self, sample):
         prob_augmentation = random.random()
         if prob_augmentation > 0.1:
             augmentation_strength = int(np.random.uniform(10, 30))
             if sample['image'].shape[0] < 360 or sample['image'].shape[1] < 480:
-                composed_transforms = transforms.Compose([
-                    tr.Resize((360, 480)),
-                    tr.RandAugment(3, augmentation_strength),
-                    tr.LabelMapping(self.label_mapping),
-                    tr.ToTensor()])
+
+                 composed_transforms = transforms.Compose([tr.Resize((360, 480)),
+                                                          tr.RandAugment(3, augmentation_strength),
+                                                          tr.LabelMapping(self.label_mapping), tr.ToTensor()])
+
             else:
-                composed_transforms = transforms.Compose([
-                    tr.RandomCrop((360, 480)),
-                    tr.RandAugment(3, augmentation_strength),
-                    tr.LabelMapping(self.label_mapping),
-                    tr.ToTensor()])
+                composed_transforms = transforms.Compose([RandomCrop((360, 480)),
+                                                          tr.RandAugment(3, augmentation_strength),
+                                                          tr.LabelMapping(self.label_mapping),
+                                                          tr.ToTensor()])
         else:
-            composed_transforms = transforms.Compose([
-                tr.RandomCrop((360, 480)),
-                tr.LabelMapping(self.label_mapping),
-                tr.ToTensor()])
+            composed_transforms = transforms.Compose([tr.RandomCrop((360, 480)),
+                                                      tr.LabelMapping(self.label_mapping),
+                                                      tr.ToTensor()])
         return composed_transforms(sample)
 
     def transforms_valid(self, sample):
-        composed_transforms = transforms.Compose([
-            tr.Resize((360, 480)),
-            tr.LabelMapping(self.label_mapping),
-            tr.ToTensor()])
+        composed_transforms = transforms.Compose([tr.Resize((360, 480)),
+                                                  tr.LabelMapping(self.label_mapping),
+                                                  tr.ToTensor()])
         return composed_transforms(sample)
 
+if __name__ == "__main__":
 
-    # from dataloaders import custom_transforms as tr
-    # from dataloaders.utils import decode_segmap
     from torch.utils.data import DataLoader
     from torchvision import transforms
-    import matplotlib.pyplot as plt
-    #import argparse
-    import os
-if __name__ == "__mapillary_load__":
+    import argparse
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data-dir", type=str, default='./datasets/data/Mapillary/',
-                        help="Path to the directory containing the PASCAL VOC dataset.")
-    parser.add_argument("--label-mapping-config", type=str, default='./datasets/data/label_mapping_config.yaml',
-                        help="Path to the label mapping config yaml")
+    parser.add_argument("--data-dir", type = str, help = "Path to the directory containing dataset")
+    parser.add_argument("--label-mapping-config", type = str, help = "path to label mapping config yaml")
 
     args = parser.parse_args()
-    train_dataset = MVD(osp.join(args.data-dir, 'training'), args.label_mapping_config)
-    trainloader = data.DataLoader(train_dataset, shuffle=True, batch_size=1,
-                                  num_workers=4, pin_memory=True)
+
+    train_dataset = Mapillary(osp.join(args.data_dir, 'training'), args.label_mapping_config)
+    trainloader = data.DataLoader(train_dataset, shuffle='True', batch_size=1,num_workers=4,pin_memory=True)
+
     cv2.namedWindow("image")
-    # cv2.namedWindow("labels")
+
     trainloader_enu = enumerate(trainloader)
 
     for step in range(10):
@@ -114,20 +125,16 @@ if __name__ == "__mapillary_load__":
         batch = next(trainloader_enu)
 
         index, sample = batch
-
         label = sample["label"]
         image = sample["image"]
 
 
-        # if index % 100 == 0:
-        #     print('%d processd' % (index))
 
-        # image, label, size = batch
         image = np.array(image[0]).astype(np.uint8)
         label = np.array(label[0]).astype(np.uint8)
 
-        image = image.transpose((1, 2, 0))
+        image = image.transpose((1,2,0))
+
         cv2.imshow("image", image)
-        # # cv2.imshow("labels", label)
-        #
-        cv2.waitKey(1500)
+
+        cv2.waitKey()
