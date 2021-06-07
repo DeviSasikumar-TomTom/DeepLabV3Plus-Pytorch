83c4dfc9060a0d695394ed5d80b5ebb88e6c4800
DeviSasikumar-TomTom <devi.sasikumar@tomtom.com>
2021-06-07 17:08:58 +0200

    My files

--- dataset1/__init__.py
+++ dataset1/__init__.py
@@ -1,4 +1,5 @@
 from .cityscapes import Cityscapes
 from .My_mapillary_loader import Mapillary
+from .transformations import RandAugment
 from .transformations import RandomCrop
 from .My_mapillary_loader import Mapillary
